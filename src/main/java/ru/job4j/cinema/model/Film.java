a112dd7db83ddc67b7789306dd68ddb097043813
dimitriy <rhymister19@gmail.com>
2025-10-30 21:05:02 +0300

    create entity models

---
+++ src/main/java/ru/job4j/cinema/model/Film.java
@@ -0,0 +1,140 @@
+package ru.job4j.cinema.model;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class Film {
+
+    public static final Map<String, String> COLUMN_MAPPING = Map.of(
+            "id", "id",
+            "name", "name",
+            "description", "description",
+            "release_year", "releaseYear",
+            "genre_id", "genreId",
+            "minimal_age", "minimalAge",
+            "duration_in_minutes", "durationInMinutes",
+            "file_id", "fileId"
+    );
+
+    private int id;
+    private String name;
+    private String description;
+    private int releaseYear;
+    private int genreId;
+    private int minimalAge;
+    private int durationInMinutes;
+    private int fileId;
+
+    private Genre genre;
+
+    public Film() {
+    }
+
+    public Film(int id, String name, String description, int releaseYear, int genreId, int minimalAge, int durationInMinutes, int fileId) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.releaseYear = releaseYear;
+        this.genreId = genreId;
+        this.minimalAge = minimalAge;
+        this.durationInMinutes = durationInMinutes;
+        this.fileId = fileId;
+    }
+
+    public Film(int id, String name, String description, int releaseYear, int genreId, int minimalAge, int durationInMinutes, int fileId, Genre genre) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.releaseYear = releaseYear;
+        this.genreId = genreId;
+        this.minimalAge = minimalAge;
+        this.durationInMinutes = durationInMinutes;
+        this.fileId = fileId;
+        this.genre = genre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public int getGenreId() {
+        return genreId;
+    }
+
+    public void setGenreId(int genreId) {
+        this.genreId = genreId;
+    }
+
+    public int getMinimalAge() {
+        return minimalAge;
+    }
+
+    public void setMinimalAge(int minimalAge) {
+        this.minimalAge = minimalAge;
+    }
+
+    public int getDurationInMinutes() {
+        return durationInMinutes;
+    }
+
+    public void setDurationInMinutes(int durationInMinutes) {
+        this.durationInMinutes = durationInMinutes;
+    }
+
+    public int getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(int fileId) {
+        this.fileId = fileId;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Film film = (Film) o;
+        return id == film.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
